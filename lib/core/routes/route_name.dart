8fbd863b2418b4fe8c63fcfa603f231fcad55c41
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-05-12 11:54:52 +0600

    theme added

---
+++ lib/core/routes/route_name.dart
@@ -0,0 +1,7 @@
+class RouteName{
+  /// variable should be static const
+  /// first character of the variable should be capitalize
+  /// variable name and the path name should be same
+  /// example : static const String home = '/home';
+  ///       or, static const String homeScreen = '/homeScreen';
+}
---
+++ lib/core/theme/theme_extension/input_decoration_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../theme_part/app_colors.dart';
+
+class AppInputDecorationTheme {
+  static InputDecorationTheme inputDecorationTheme = InputDecorationTheme(
+    hintStyle: TextStyle(color: Color(0xffB4B4B4)),
+    prefixIconColor: Color(0xffB4B4B4),
+    suffixIconColor: Color(0xffB4B4B4),
+    errorStyle: TextStyle(color: AppColors.error),
+    border: OutlineInputBorder(
+      borderSide: BorderSide(color: AppColors.onSecondary),
+      borderRadius: BorderRadius.circular(10.r),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: AppColors.onSecondary),
+      borderRadius: BorderRadius.circular(10.r),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: AppColors.primary),
+      borderRadius: BorderRadius.circular(10.r),
+    ),
+    disabledBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: AppColors.onSecondary),
+      borderRadius: BorderRadius.circular(10.r),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: AppColors.error),
+      borderRadius: BorderRadius.circular(10.r),
+    ),
+  );
+}
