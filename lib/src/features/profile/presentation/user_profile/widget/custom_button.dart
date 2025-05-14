0dddbd7a61969c8527845d5459dd647ce7395851
df <fd>
2025-05-14 16:08:19 +0600

    pull ok

---
+++ lib/src/features/profile/presentation/user_profile/widget/custom_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Mybutton extends StatelessWidget {
+  final String text;
+  final Color color;
+  final void Function()? onTap;
+  const Mybutton({super.key,
+  required this.color,
+  required this.text,
+  required this.onTap
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height:56.h,
+        width: 158.w,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.r),
+          color: color
+        ),
+        child: Center(
+          child: Text(text,
+          style: Theme.of(context).textTheme.titleSmall!.copyWith(
+            color: Color(0xffffffff),
+            fontWeight: FontWeight.w700,
+          ),
+          ),
+        ),
+      ),
+    );
+  }
+}
