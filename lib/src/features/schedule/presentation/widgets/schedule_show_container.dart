ff08e81262be7ca94610cb69dd2e5ccabc2e3228
nahidulislamshakin <shakinhabib2000@gmail.com>
2025-05-23 12:11:42 +0600

    add review button added

--- lib/src/features/schedule/presentation/widgets/schedule_show_container.dart
+++ lib/src/features/schedule/presentation/widgets/schedule_show_container.dart
@@ -3,6 +3,7 @@ import 'package:e_learning_app/core/theme/theme_part/app_colors.dart';
 import 'package:e_learning_app/core/utils/common_widget.dart';
 import 'package:e_learning_app/core/utils/utils.dart';
 import 'package:e_learning_app/src/features/schedule/model/meeting_model.dart';
+import 'package:e_learning_app/src/features/schedule/presentation/widgets/schedule_show_container_footer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
@@ -28,6 +29,7 @@ class ScheduleShowContainer extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
+          /// Profile Picture
           ClipOval(
             child: Image.asset(
               meetingScheduleModel.profilePicture,
@@ -39,10 +41,12 @@ class ScheduleShowContainer extends StatelessWidget {
 
           SizedBox(height: 16.h),
 
+          /// User Name
           Text(meetingScheduleModel.userName, style: textTheme.titleMedium),
 
           SizedBox(height: 4.h),
 
+          /// Designation
           Text(
             meetingScheduleModel.designation,
             style: textTheme.bodyMedium?.copyWith(
@@ -52,101 +56,9 @@ class ScheduleShowContainer extends StatelessWidget {
 
           SizedBox(height: 4.h),
 
-          meetingScheduleModel.status != "canceled"
-              ?
-          Column(
-                spacing: 12.h,
-                children: [
-                  Row(
-                    spacing: 8.w,
-                    children: [
-                      SvgPicture.asset(AppIcons.calendar),
-                      Text(
-                        meetingScheduleModel.scheduleDate,
-                        style: textTheme.bodyMedium?.copyWith(
-                          color: AppColors.secondaryTextColor,
-                        ),
-                      ),
-                      Spacer(),
-                      if (meetingScheduleModel.status == "completed")
-                        Text(
-                          "30 Min",
-                          style: textTheme.labelMedium?.copyWith(
-                            color: Color(0xffD2D2D5),
-                          ),
-                        ),
-                    ],
-                  ),
-
-                  meetingScheduleModel.status != "completed"
-                      ? Row(
-                        spacing: 8.w,
-                        children: [
-                          Expanded(
-                            child: CommonWidget.primaryButton(
-                              textStyle: buttonTextStyle,
-                              context: context,
-                              onPressed: () {},
-                              text: "Cancel",
-                              backgroundColor: Color(0xff2B2C31),
-                            ),
-                          ),
-                          Expanded(
-                            child: CommonWidget.primaryButton(
-                              backgroundColor:
-                                  meetingScheduleModel.status == "pending"
-                                      ? Color(0xff4A4C56)
-                                      : AppColors.primary,
-                              textStyle: buttonTextStyle?.copyWith(
-                                color:
-                                    meetingScheduleModel.status == "pending"
-                                        ? Color(0xffA5A5AB)
-                                        : Colors.white,
-                              ),
-                              context: context,
-                              onPressed: () {},
-                              text: "Copy Link",
-                            ),
-                          ),
-                        ],
-                      )
-                      : SizedBox(
-                        width: double.infinity,
-                        child: CommonWidget.primaryButton(
-                          backgroundColor: Color(0xff2B2C31),
-                          context: context,
-                          onPressed: () {},
-                          text: "View Summary",
-                          textStyle: buttonTextStyle,
-                        ),
-                      ),
-                ],
-              )
-              :
-          Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 12.h,
-                children: [
-                  Text(
-                    "Cancelled The Meeting",
-                    style: textTheme.bodyMedium?.copyWith(
-                      color: AppColors.error,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-
-                  SizedBox(
-                    width: double.infinity,
-                    child: CommonWidget.primaryButton(
-                      context: context,
-                      onPressed: () {},
-                      text: "Refund",
-                      textStyle: buttonTextStyle,
-                      backgroundColor: AppColors.error,
-                    ),
-                  ),
-                ],
-              ),
+          ScheduleShowContainerFooter(
+            meetingScheduleModel: meetingScheduleModel,
+          ),
         ],
       ),
     );
