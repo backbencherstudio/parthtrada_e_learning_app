6e9344b340936cfc0a4f3cfcfe3c9efee018a21b
Asif-Rezan <asifrezan.office@gmail.com>
2025-09-27 16:21:14 +0600

    update profile completed

--- lib/src/features/profile/data/repository/profile_repository_impl.dart
+++ lib/src/features/profile/data/repository/profile_repository_impl.dart
@@ -8,7 +8,8 @@ import '../../../../../core/services/api_services/api_services.dart';
 class ProfileRepositoryImpl extends ProfileRepository {
   final ApiService _apiService;
 
-  ProfileRepositoryImpl({ApiService? apiService}) : _apiService = apiService ?? ApiService();
+  ProfileRepositoryImpl({ApiService? apiService})
+    : _apiService = apiService ?? ApiService();
 
   @override
   Future<ProfileResponseData> getProfileInfo() async {
@@ -16,7 +17,9 @@ class ProfileRepositoryImpl extends ProfileRepository {
       final response = await _apiService.get(ApiEndPoints.profileInfo);
       debugPrint("Profile data response: ${response.data}");
       if (response.data != null && response.data is Map<String, dynamic>) {
-        return ProfileResponseData.fromJson(response.data as Map<String, dynamic>);
+        return ProfileResponseData.fromJson(
+          response.data as Map<String, dynamic>,
+        );
       } else {
         return ProfileResponseData(
           success: false,
@@ -29,6 +32,37 @@ class ProfileRepositoryImpl extends ProfileRepository {
         success: false,
         message: "Error fetching profile: ${e.toString()}",
       );
+    }
+  }
+
+  @override
+  Future<bool> updateProfile(
+    String profession,
+    String organization,
+    String location,
+    String description,
+  ) async {
+    final data = {
+      'profession': profession,
+      'organization': organization,
+      'location': location,
+      'description': description,
+    };
+
+    try {
+      final response = await _apiService.patch(
+        ApiEndPoints.updateProfile,
+        data: data,
+      );
+
+      if (response.statusCode == 200 && response.data['success'] == true) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      debugPrint("Error fetching profile info: $e");
+      return false;
     }
   }
 }
