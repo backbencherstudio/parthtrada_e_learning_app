8b8573e2bca7da45591abea9658fdc5aa1807bac
Rawnak003 <shmrawnak@gmail.com>
2025-10-23 10:41:36 +0600

    fix: booking session

--- lib/src/features/book_expert/presentation/schedule_for_book/confirm_booking_bottom_sheet/confirm_booking_bottom_sheet.dart
+++ lib/src/features/book_expert/presentation/schedule_for_book/confirm_booking_bottom_sheet/confirm_booking_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:e_learning_app/core/constant/icons.dart';
 import 'package:e_learning_app/core/utils/common_widget.dart';
-import 'package:e_learning_app/src/features/book_expert/rvierpod/session_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -9,9 +8,8 @@ import 'package:go_router/go_router.dart';
 import 'package:shimmer/shimmer.dart';
 import '../../../../../../core/constant/padding.dart';
 import '../../../../../../core/theme/theme_part/app_colors.dart';
-import '../../../../../../repository/api/expert/expert_booking.dart';
-import '../../../model/session_model.dart';
 import '../../../rvierpod/book_expert_riverpod.dart';
+import '../../../rvierpod/session_provider.dart';
 import '../payment_bottom_sheet.dart';
 import 'confirm_book_details_card.dart';
 import 'expert_booking_shimmer.dart';
@@ -22,23 +20,16 @@ Future<void> confirmAndPayBottomSheet({
 }) async {
   await showModalBottomSheet(
     backgroundColor: Colors.transparent,
-    useSafeArea: false,
+    useSafeArea: true,
     isScrollControlled: true,
     context: context,
     builder: (bottomSheetContext) {
       final textTheme = Theme.of(bottomSheetContext).textTheme;
-      final buttonTextStyle = textTheme.titleMedium?.copyWith(
-        fontWeight: FontWeight.w700,
-      );
-
       return Consumer(
         builder: (context, ref, _) {
-          final sessionData = ref.read(sessionDataProvider);
-          final bookExpertNotifier = ref.read(
-            bookExpertRiverpod(availableTime).notifier,
-          );
+          final sessionData = ref.watch(sessionDataProvider);
           final bookExpertState = ref.watch(bookExpertRiverpod(availableTime));
-
+          final bookExpertNotifier = ref.read(bookExpertRiverpod(availableTime).notifier);
           return Container(
             constraints: BoxConstraints(maxHeight: 585.h, minHeight: 440.h),
             padding: AppPadding.screenHorizontal,
@@ -49,63 +40,62 @@ Future<void> confirmAndPayBottomSheet({
                 topRight: Radius.circular(32.r),
               ),
             ),
-            child:
-                bookExpertState.isConfirmLoading
-                    ? Center(
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: Shimmer.fromColors(
-                          baseColor: AppColors.secondary,
-                          highlightColor: AppColors.screenBackgroundColor,
-                          child: ExpertBookingShimmer(),
+            child: bookExpertState.isConfirmLoading
+                ? Center(
+              child: Shimmer.fromColors(
+                baseColor: AppColors.secondary,
+                highlightColor: AppColors.screenBackgroundColor,
+                child: const ExpertBookingShimmer(),
+              ),
+            )
+                : SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: 16.h),
+                  Text(
+                    "Confirm Booking",
+                    style: textTheme.bodyMedium?.copyWith(
+                      color: AppColors.textColor,
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 12.h),
+                  ConfirmBookDetailsCard(),
+                  SizedBox(height: 32.h),
+                  SafeArea(
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: CommonWidget.primaryButton(
+                        padding: EdgeInsets.symmetric(vertical: 16.h),
+                        textStyle: textTheme.titleMedium?.copyWith(
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
                         ),
-                      ),
-                    )
-                    : SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          SizedBox(height: 16.h),
-                          Text(
-                            "Confirm Booking",
-                            style: textTheme.bodyMedium?.copyWith(
-                              color: AppColors.textColor,
-                              fontSize: 18.sp,
-                              fontWeight: FontWeight.w600
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: 12.h),
-                          ConfirmBookDetailsCard(),
-                          SizedBox(height: 32.h),
-                          SafeArea(
-                            child: SizedBox(
-                              width: double.infinity,
-                              child: CommonWidget.primaryButton(
-                                padding: EdgeInsets.symmetric(vertical: 16.h),
-                                textStyle: buttonTextStyle,
-                                context: bottomSheetContext,
-                                text: "Confirm & Pay",
-                                onPressed: () async {
-                                  await bookExpertNotifier.onConfirmBooking();
-                                  debugPrint("Confirm Booking");
-
-                                  Navigator.pop(bottomSheetContext);
-
-                                  Future.delayed(
-                                    const Duration(milliseconds: 200),
-                                    () {
-                                      paymentBottomSheet(context: context, availableTime: []);
-                                    },
-                                  );
-                                },
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 28.h),
-                        ],
+                        context: bottomSheetContext,
+                        text: "Confirm & Pay",
+                        backgroundColor: AppColors.primary,
+                        onPressed: () {
+                          Future.microtask(() async {
+                            await bookExpertNotifier.onConfirmBooking();
+                            if (bottomSheetContext.mounted) {
+                              bottomSheetContext.pop();
+                              await Future.delayed(const Duration(milliseconds: 200));
+                              if (bottomSheetContext.mounted) {
+                                await paymentBottomSheet(context: bottomSheetContext, availableTime: availableTime);
+                              }
+                            }
+                          });
+                        },
                       ),
                     ),
+                  ),
+                  SizedBox(height: 28.h),
+                ],
+              ),
+            ),
           );
         },
       );
--- lib/src/features/book_expert/presentation/schedule_for_book/payment_bottom_sheet.dart
+++ lib/src/features/book_expert/presentation/schedule_for_book/payment_bottom_sheet.dart
@@ -9,6 +9,7 @@ import '../../../../../core/routes/route_name.dart';
 import '../../rvierpod/get_card_notifier.dart';
 import '../../rvierpod/payment_provider.dart';
 import 'confirm_booking_bottom_sheet/confirm_and_pay_bottom_sheet.dart';
+import 'confirm_booking_bottom_sheet/confirm_booking_bottom_sheet.dart';
 
 class PaymentConstants {
   static const selectPaymentMethod = "Select your payment method to proceed";
@@ -24,12 +25,14 @@ Future<void> paymentBottomSheet({
 }) async {
   await showModalBottomSheet(
     backgroundColor: Colors.transparent,
+    useSafeArea: true,
     isScrollControlled: true,
     context: context,
     builder: (bottomSheetContext) {
       final textTheme = Theme.of(bottomSheetContext).textTheme;
       final buttonTextStyle = textTheme.titleMedium?.copyWith(
         fontWeight: FontWeight.w700,
+        color: Colors.white,
       );
 
       return Consumer(
@@ -37,9 +40,7 @@ Future<void> paymentBottomSheet({
           final cardState = ref.watch(cardProvider);
 
           // Fetch cards once if not yet fetched
-          if (!cardState.isLoading &&
-              cardState.cardsResponse == null &&
-              cardState.error == null) {
+          if (!cardState.isLoading && cardState.cardsResponse == null && cardState.error == null) {
             Future.microtask(() => ref.read(cardProvider.notifier).fetchCards());
           }
 
@@ -66,50 +67,32 @@ Future<void> paymentBottomSheet({
                   textAlign: TextAlign.center,
                 ),
                 SizedBox(height: 16.h),
-
-                // Card List
                 Expanded(
                   child: cardState.isLoading
-                      ? const Center(
-                    child: CircularProgressIndicator(
-                      color: AppColors.primary,
-                    ),
-                  )
+                      ? const Center(child: CircularProgressIndicator(color: AppColors.primary))
                       : cardState.error != null
                       ? Center(
                     child: Text(
                       cardState.error!,
-                      style: textTheme.bodyMedium?.copyWith(
-                        color: AppColors.error,
-                      ),
+                      style: textTheme.bodyMedium?.copyWith(color: AppColors.error),
                     ),
                   )
                       : (cardState.cardsResponse?.data.isEmpty ?? true)
                       ? Center(
                     child: Text(
                       PaymentConstants.noCardsFound,
-                      style: textTheme.bodyMedium?.copyWith(
-                        color: AppColors.secondaryTextColor,
-                      ),
+                      style: textTheme.bodyMedium?.copyWith(color: AppColors.secondaryTextColor),
                     ),
                   )
                       : ListView.builder(
-                    itemCount:
-                    cardState.cardsResponse?.data.length ?? 0,
+                    itemCount: cardState.cardsResponse?.data.length ?? 0,
                     itemBuilder: (context, index) {
-                      final card =
-                      cardState.cardsResponse!.data[index];
-                      final isSelected =
-                          selectedMethodId == card.methodId;
-
+                      final card = cardState.cardsResponse!.data[index];
+                      final isSelected = selectedMethodId == card.methodId;
                       return GestureDetector(
                         onTap: () {
-                          ref
-                              .read(selectedCardMethodIdProvider
-                              .notifier)
-                              .state = card.methodId;
-                          debugPrint(
-                              'Selected card: ${card.methodId}');
+                          ref.read(selectedCardMethodIdProvider.notifier).state = card.methodId;
+                          debugPrint('Selected card: ${card.methodId}');
                         },
                         child: Container(
                           height: 50.h,
@@ -117,13 +100,9 @@ Future<void> paymentBottomSheet({
                           margin: EdgeInsets.only(bottom: 16.h),
                           decoration: BoxDecoration(
                             color: AppColors.surface,
-                            borderRadius:
-                            BorderRadius.circular(10.r),
+                            borderRadius: BorderRadius.circular(10.r),
                             border: isSelected
-                                ? Border.all(
-                              color: AppColors.primary,
-                              width: 2,
-                            )
+                                ? Border.all(color: AppColors.primary, width: 2)
                                 : null,
                           ),
                           child: Center(
@@ -157,29 +136,57 @@ Future<void> paymentBottomSheet({
                     },
                   ),
                 ),
-
                 SizedBox(height: 16.h),
                 SizedBox(
                   width: double.infinity,
-                  child: CommonWidget.primaryButton(
-                    padding: EdgeInsets.symmetric(vertical: 16.h),
-                    textStyle: buttonTextStyle,
-                    context: bottomSheetContext,
-                    text: (cardState.cardsResponse?.data.isEmpty ?? true) ? 'Add Your Card' : ref.watch(paymentNotifierProvider).isLoading
-                        ? PaymentConstants.paymentProcessing
-                        : PaymentConstants.paymentProceed,
-                    onPressed: () {
-                      if (cardState.cardsResponse?.data.isEmpty ?? true) {
-                        context.push(RouteName.addPaymentMethod);
-                      } else {
-                        _handlePayment(
-                          ref,
-                          bottomSheetContext,
-                          context,
-                          availableTime,
-                        );
-                      }
-                    }
+                  child: Consumer(
+                    builder: (context, ref, _) {
+                      final paymentState = ref.watch(paymentNotifierProvider);
+                      final cardState = ref.watch(cardProvider);
+                      return CommonWidget.primaryButton(
+                        padding: EdgeInsets.symmetric(vertical: 16.h),
+                        textStyle: buttonTextStyle,
+                        context: bottomSheetContext,
+                        text: (cardState.cardsResponse?.data.isEmpty ?? true)
+                            ? 'Add Your Card'
+                            : paymentState.isLoading
+                            ? PaymentConstants.paymentProcessing
+                            : PaymentConstants.paymentProceed,
+                        backgroundColor: (cardState.cardsResponse?.data.isEmpty ?? true) || paymentState.isLoading
+                            ? AppColors.secondaryStrokeColor
+                            : AppColors.primary,
+                        onPressed: (cardState.cardsResponse?.data.isEmpty ?? true) || paymentState.isLoading
+                            ? () {}
+                            : () {
+                          Future.microtask(() async {
+                            final notifier = ref.read(paymentNotifierProvider.notifier);
+                            try {
+                              final success = await notifier.confirmPayment();
+                              if (success && bottomSheetContext.mounted) {
+                                bottomSheetContext.pop();
+                                await Future.delayed(const Duration(milliseconds: 200));
+                                if (bottomSheetContext.mounted) {
+                                  await confirmBookingBottomSheet(
+                                    context: bottomSheetContext,
+                                    availableTime: availableTime,
+                                  );
+                                }
+                              } else if (bottomSheetContext.mounted) {
+                                ScaffoldMessenger.of(bottomSheetContext).showSnackBar(
+                                  const SnackBar(content: Text(PaymentConstants.paymentFailed)),
+                                );
+                              }
+                            } catch (e) {
+                              if (bottomSheetContext.mounted) {
+                                ScaffoldMessenger.of(bottomSheetContext).showSnackBar(
+                                  SnackBar(content: Text("Error: $e")),
+                                );
+                              }
+                            }
+                          });
+                        },
+                      );
+                    },
                   ),
                 ),
                 SizedBox(height: 28.h),
@@ -190,41 +197,4 @@ Future<void> paymentBottomSheet({
       );
     },
   );
-}
-
-Future<void> _handlePayment(
-    WidgetRef ref,
-    BuildContext bottomSheetContext,
-    BuildContext context,
-    List<String> availableTime,
-    ) async {
-  final notifier = ref.read(paymentNotifierProvider.notifier);
-
-  try {
-    final success = await notifier.confirmPayment();
-
-    if (success && bottomSheetContext.mounted) {
-      Navigator.of(bottomSheetContext).pop();
-      await Future.delayed(const Duration(milliseconds: 200));
-
-      if (context.mounted) {
-        await confirmBookingBottomSheet(
-          context: context,
-          availableTime: availableTime,
-        );
-      }
-    } else if (context.mounted) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text(PaymentConstants.paymentFailed),
-        ),
-      );
-    }
-  } catch (e) {
-    if (context.mounted) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Error: $e")),
-      );
-    }
-  }
 }
